8b3c8fa776081cbf47f1ce530b2cfb14ed84e953
khushi <khusbumandal2060@gmail.com>
2025-08-29 23:48:02 +0545

    assets

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,7 +3,12 @@ import './Weather.css'
 const Weather = () => {
   return (
     <div>
-      <h2>Weather</h2>
+      <div className='weather'>
+        <div className="search-bar">
+            <input type="text" placeholder='Search'></input>
+            <img src="" alt=""></img>
+        </div>
+      </div>
     </div>
   )
 }
